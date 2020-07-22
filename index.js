42adabada52e354acfae2f87f5ffd7e92e01b500
shivam <shivam@gramfactory.com>
2020-07-22 13:21:31 +0530

    done

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require("path")
 
-const routes = require('./routes');
+const usersRouter = require('./routes/auth');
+const pollsRouter = require('./routes/poll');
+
+
 const handle = require('./handlers');
 
 const app = express();
@@ -14,8 +17,8 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/api/auth', routes.auth);
-app.use('/api/polls', routes.poll);
+app.use('/api/users', usersRouter);
+app.use('/api/polls', pollsRouter);
 
 app.use((req, res, next) => {
   let err = new Error('Not Found');
@@ -27,11 +30,11 @@ app.use(handle.error);
 
 
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
     app.use(express.static('client/build'));
-    app.get(/^\/(?!api).*/, (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/build/index.html'));
-  });
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname + '/client/build/index.html'));
+    });
 }
 
 
