c3ed02a74152fe469690305254fa8b84e33f4831
shivam <shivam@gramfactory.com>
2020-07-22 13:24:48 +0530

    done

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(handle.error);
 
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
     app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
+    app.get('/', (req, res) => {
         res.sendFile(path.join(__dirname + '/client/build/index.html'));
     });
 }
